d1da75214ec86177c66c5b65a1b9797260994280
unknown <ben.rappoport@gmail.com>
2025-06-06 11:24:37 -0400

    rectangle loop almost working

--- rectangleLoop.py
+++ rectangleLoop.py
@@ -1,10 +1,5 @@
 import random
-from matrixPrint import matrixPrint 
-def makeRow(index, matrix, length):
-    row = []
-    for i in range(length):
-        row.append(matrix[i][index])
-    return row
+# from matrixPrint import matrixPrint 
 
 def rectabgleLoop(matrix, iterations):
     for i in range(int(iterations)):
@@ -16,13 +11,13 @@ def rectabgleLoop(matrix, iterations):
         # print("colum_num= " + str(colum_num))
         # print("Num picked= " + str(firstNum))
         if firstNum == 1:
-            row = makeRow(colum_num, matrix, len(matrix))
+            # row = makeRow(colum_num, matrix, len(matrix))
             secondNum = -1
-            for i in row:
-                if i == 0:
+            for i in range(len(matrix)):
+                if matrix[i][row_num] == 0:
                     secondNum = i
             if secondNum == -1:
-                print("no second number in row")
+                # print("no second number in row")
                 continue
             second_colum = matrix[secondNum]
 
@@ -31,22 +26,24 @@ def rectabgleLoop(matrix, iterations):
                 if i == 1:
                     thirdNum = i
             if thirdNum == -1:
-                print("No third number in colum")
+                # print("No third number in colum")
                 continue
 
             #this final part discovers if it is a checherboard
             if matrix[colum_num][thirdNum] == 0:
                 #now to swap THIS REALLY SHOULD BE CHECKED
-                print("it found a match")
-                matrixPrint(matrix)
-                print(str(colum_num)+"," + str(row_num))
-                print(str(secondNum)+"," + str(thirdNum))
-                print(str(secondNum)+"," + str(row_num))
-                print(str(colum_num)+","+str(thirdNum))
+                # print("it found a match")
+                # matrixPrint(matrix)
+                # print(str(colum_num)+"," + str(row_num))
+                # print(str(secondNum)+"," + str(thirdNum))
+                # print(str(secondNum)+"," + str(row_num))
+                # print(str(colum_num)+","+str(thirdNum))
 
                 matrix[colum_num][row_num] = 0
                 matrix[secondNum][thirdNum] = 0
                 matrix[secondNum][row_num] = 1
                 matrix[colum_num][thirdNum] = 1
+                # matrixPrint(matrix)
+    return matrix
 
         
