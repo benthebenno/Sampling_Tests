ff2c14aa96c188b48588bb8ee3c45dcd1a8ec3f7
unknown <ben.rappoport@gmail.com>
2025-06-12 12:19:17 -0400

    working on it

--- initMaker.py
+++ initMaker.py
@@ -1,4 +1,6 @@
 from tests import matrixPrint
+#taken from here https://www.geeksforgeeks.org/generate-a-matrix-with-each-row-and-column-of-given-sum/
+
 def initMaker(x_array, y_array):
 
     rows, cols = (len(x_array), len(y_array))
@@ -7,20 +9,18 @@ def initMaker(x_array, y_array):
     # print(f"x Array = {x_array}")
     # print(f"y array = {y_array}")
     # matrixPrint(matrix)
-    for i in range(len(matrix)):
-        k = 0
-        j = 0
-        while j < x_array[i]:
-            if len(y_array) <= k:
-                break
-            if(y_array[k] == 0):
-                j = j - 1
-                k = k + 1
-            else: 
-                matrix[i][k] = 1
-                y_array[k] = int(y_array[k]) - 1
-                k = k + 1
-            j = j + 1
+
+    currRowSum = [0] * rows
+    currColSum = [0] * cols    
+    for i in range(rows):
+        for j in range(cols):
+            rowVal = x_array[i] - currRowSum[i]
+            colVal = y_array[j] - currColSum[j]
+
+            matrix[i][j] = min(rowVal, colVal, 1)
+
+            currRowSum[i] += matrix[i][j]
+            currColSum[j] += matrix[i][j]
     return matrix 
 
 
