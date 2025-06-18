32c76c96464b1a6954f750f380194dd2320965f2
unknown <ben.rappoport@gmail.com>
2025-06-18 12:33:09 -0400

    update

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-array = [1, 2, 3]
-tempArray = array.copy()
-tempArray[0] = 40000
-print(array)
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
+
+npArray = np.zeros((5,5))
+
+f, ( (ax7)) = plt.subplots(1, 1, sharex='col', sharey='row')
+ax7.imshow(np.real(npArray), cmap=cm.gray)
