ea40d778a00003a2317045868a4e99c30783012c
unknown <ben.rappoport@gmail.com>
2025-06-18 11:32:01 -0400

    stuck on rounding

--- swapFourier.py
+++ swapFourier.py
@@ -8,14 +8,6 @@ import matplotlib.cm as cm
 from PIL import Image
 from initTest import matrixSum
 
-def roundMatrix(matrix):
-    print(matrix)
-    for x in range(len(matrix)):
-        for y in range(len(matrix[x])):
-            if matrix > .5:
-                matrix[x, y] = 0
-            else: 
-                matrix[x,y] = 1
 
 def fourSwap(x_sums, y_sums, iterations, im):
 
@@ -23,19 +15,19 @@ def fourSwap(x_sums, y_sums, iterations, im):
     partialMatrix = fourier0sMaker(x_sums, y_sums, im)
     # This part should do part 1 of the algorithm
     np_matrix = np.zeros((len(x_sums), len(y_sums)))
-
+    for sumNum in range(len(x_sums)):
+                yIndex = sumNum
+                count = 0
+                while x_sums[sumNum] > 0:
+                    if count == len(x_sums):
+                        count = 0
+                    if np_matrix[yIndex, count] == 0: 
+                        if random.randrange(0,len(x_sums)) == 0:
+                            np_matrix[yIndex, count ] = 1
+                            x_sums[sumNum] -= 1
+                    count += 1
     for i in range(iterations):
-        for sumNum in range(len(x_sums)):
-            yIndex = sumNum
-            count = 0
-            while x_sums[sumNum] > 0:
-                if count == len(x_sums):
-                    count = 0
-                if np_matrix[yIndex, count] == 0: 
-                    if random.randrange(0,len(x_sums)) == 0:
-                        np_matrix[yIndex, count ] = 1
-                        x_sums[sumNum] -= 1
-                count += 1
+       
         # print(np_matrix)
 
         fourMatrix = fftn(np_matrix)
@@ -43,14 +35,32 @@ def fourSwap(x_sums, y_sums, iterations, im):
             fourMatrix[(x,0)] = partialMatrix[(x,0)]
         for y in range(len(fourMatrix[0])):
             fourMatrix[(0,y)] = partialMatrix[(0,y)]
-        np_matrix = (ifftn(fourMatrix))
 
-    return ifftn(fourMatrix)
+        #This turns the next matrix into the old one 
+
+        transformedMatrix = np.real(ifftn(fourMatrix))
+
+        for x in range(len(transformedMatrix)):
+            for y in range(len(transformedMatrix[x])):
+                if transformedMatrix[(x,y)] > 0:
+                    transformedMatrix[x,y] = 1
+                else:   
+                    transformedMatrix[x,y] = 0
+        
+        #This line makes the next iteration the ROUNDED transformed matrix
+        np_matrix = transformedMatrix
+
+        print("TRAnsform heading")
+        print(transformedMatrix)
+        print("-----------------------------")
+        print("Original heading")
+        print(ifftn(fourMatrix))
+    return (transformedMatrix, ifftn(fourMatrix) )
 
 
         
 def main():
-    f, ((ax1), (ax2), (ax6), (ax3), (ax5)) = plt.subplots(5, 1, sharex='col', sharey='row')
+    f, ((ax1), (ax2), (ax6), (ax3), (ax5), (ax7)) = plt.subplots(6, 1, sharex='col', sharey='row')
 
     imagePath = input("Please give an image path: ")
     im = Image.open(imagePath)
@@ -71,16 +81,33 @@ def main():
                 np_matrix[x,y] = 1
 
     (x_val, y_val) = matrixSum(np_matrix)
-    print(x_val,y_val)
+    # print(x_val,y_val)
     
+
+    sawpTouple = fourSwap(x_val, y_val, iterations, im)
+
+    #The original image
     ax1.imshow(np_matrix, cmap=cm.gray)
+
+    #The fftn of the original matrix
     ax2.imshow(np.real(fftn(np_matrix)), cmap = cm.gray)
+
+    #The correct reconstruction (should always be identical to the original image)
     ax6.imshow(np.real(ifftn(fftn(np_matrix))), cmap = cm.gray)
+
+    #The edge fourier transform
     ax3.imshow(np.real(fourier0sMaker(x_val, y_val, im)), cmap = cm.gray)
-    ax5.imshow(np.real(fourSwap(x_val, y_val, iterations, im)), cmap=cm.gray)
-    print(fourSwap(x_val, y_val, iterations, im))
-    print("break")
-    print(np.real(fourSwap(x_val, y_val, iterations, im)))
+
+    #rouned data
+    ax5.imshow(np.real(sawpTouple[0]), cmap=cm.gray)
+
+    #data before rounding
+    ax7.imshow(np.real(sawpTouple[1]), cmap=cm.gray)
+
+
+    # print(fourSwap(x_val, y_val, iterations, im))
+    # print("break")
+    # print(np.real(fourSwap(x_val, y_val, iterations, im)))
     
     plt.show()
 
