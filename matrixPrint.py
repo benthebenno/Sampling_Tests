01d0531b5aee7e724dda0fb14b845f7114dc9683
Benjamin Rappoport <ben.rappoport@gmail.com>
2025-07-16 12:40:06 -0400

    removed all unessicary files

--- matrixPrint.py
+++
@@ -1,7 +0,0 @@
-def matrixPrint(matrix):
-    for i in range(len(matrix)):
-        for m in matrix:
-            # print(i)
-            print(m[i], end="")
-        print("")
-        
--- minitest.py
+++
@@ -1,5 +0,0 @@
-matrix = [3, 2, 34, 4, 4]
-
-for x in range(-len(matrix)//2, len(matrix)//2):
-    for y in range(-len(matrix)//2, len(matrix)//2):
-        print(x,y)
